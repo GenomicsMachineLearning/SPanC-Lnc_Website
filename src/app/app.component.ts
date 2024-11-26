a4df3b712dc190535ef79fe1397627fca70ae379
Andrew Newman <andrewfnewman@gmail.com>
2024-11-26 10:12:40 +1000

    Removed unused components.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,11 +4,8 @@ import { Component, OnInit } from '@angular/core';
   selector: 'ngx-app',
   template: '<router-outlet></router-outlet>',
 })
-export class AppComponent implements OnInit {
+export class AppComponent {
 
   constructor() {
-  }
-
-  ngOnInit(): void {
   }
 }
