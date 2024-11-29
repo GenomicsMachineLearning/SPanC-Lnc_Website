84371c18889112c69af0b2fd129c6587d7af26cd
Andrew Newman <andrewfnewman@gmail.com>
2024-11-30 09:06:34 +1000

    Try new GET.

--- src/app/pages/atlas/atlas.component.ts
+++ src/app/pages/atlas/atlas.component.ts
@@ -2,6 +2,8 @@ import {Component} from '@angular/core';
 import {FormControl} from '@angular/forms';
 import {debounceTime} from 'rxjs/operators';
 import {GeneExplorerService} from "../../../service/gene-explorer.service";
+import {environment} from "../../../environments/environment";
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
   selector: 'ngx-atlas',
@@ -12,22 +14,27 @@ export class AtlasComponent {
   public sampleGenes: any = [
     {
       "image": "assets/images/geneExporer/headaneck.jpeg",
+      "result": "",
       "name": 'Head and Neck Cancer'
     },
     {
       "image": "assets/images/geneExporer/Melanoma.png",
+      "result": "",
       "name": 'Melanoma'
     },
     {
       "image": "assets/images/geneExporer/SCC.png",
+      "result": "",
       "name": 'SCC'
     },
     {
       "image": "assets/images/geneExporer/BCC.png",
+      "result": "",
       "name": 'BCC'
     },
     {
       "image": "assets/images/geneExporer/KidneyCancer.png",
+      "result": "",
       "name": 'Kidney Cancer'
     }
   ];
@@ -37,26 +44,28 @@ export class AtlasComponent {
   public showNotFound: boolean = true
   searchControl = new FormControl();
 
-  constructor(private geneService: GeneExplorerService) {
+  constructor(private sanitizer: DomSanitizer) {
     this.searchControl.valueChanges
       .pipe(debounceTime(1000)) // Adjust debounce time as per your requirement
       .subscribe(newValue => {
         // This code will execute after 300ms of user input pause
         if (newValue) {
-          var data: any = {};
-          data.cutarId = newValue?.trim();
-          this.showSpinner = true;
-          this.geneService.getSamplesImg(data).subscribe((res: any) => {
-            this.sampleTissueImg = res.data
-            this.showSpinner = false;
-            this.showGenes = true;
-            this.showNotFound = false
-          }, (err) => {
-            this.showNotFound = true;
-            this.showGenes = false;
-            this.showSpinner = false;
-          })
+          const cutarId = newValue?.trim();
+          this.updateImageUrls(cutarId)
         }
-      });
+        this.showGenes = true;
+      }
+    );
+  }
+
+  constructImageUrl(cutarId: string, sampleName: string): string {
+    return `${environment.apiBaseURL}/genes?cutarId=${cutarId}&sampleName=${encodeURIComponent(sampleName)}`;
+  }
+
+  updateImageUrls(cutarId: string): void {
+    this.sampleGenes = this.sampleGenes.map(gene => ({
+      ...gene,
+      result: this.sanitizer.bypassSecurityTrustUrl((this.constructImageUrl(cutarId, gene.name)))
+    }));
   }
 }
